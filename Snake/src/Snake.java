557e7f722defc0e7baef58e927f50cb9d23b5bd0
studycat <1376253264@qq.com>
2019-03-26 22:33:02 +0800

    add class of EGG

--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -1,10 +1,15 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+
 
 public class Snake {
 	Node head = null;
 	Node tail = null;
 	private int size = 0;
+	
+	private Node n = new Node(20, 30, Dir.L);
 	
 	public Snake(Node node) {
 		head = node;
@@ -12,18 +17,106 @@ public class Snake {
 		size = 1;
 	}
 	
-	public void addToTail(Node node) {
-		
+	public Snake() {
+		head = n;
+		tail = n;
+		size = 1;
 	}
 	
+	public void addToHead() {
+		Node node = null;
+		switch (head.dir) {
+		case L:
+			node = new Node(head.row, head.col - 1, head.dir);
+			break;
+		case U:
+			node = new Node(head.row - 1, head.col, head.dir);
+			break;
+			
+		case R:
+			node = new Node(head.row, head.col + 1, head.dir);
+			break;
+			
+		case D:
+			node = new Node(head.row + 1, head.col, head.dir);
+			break;
+		}
+		
+		node.next = head;
+		head.prev = node;
+		head = node;
+		size++;
+	}
+	
+	public void addToTail() {
+		Node node = null;
+		switch (tail.dir) {
+		case L:
+			node = new Node(tail.row, tail.col + 1, tail.dir);
+			break;
+		case U:
+			node = new Node(tail.row + 1, tail.col, tail.dir);
+			break;
+			
+		case R:
+			node = new Node(tail.row, tail.col - 1, tail.dir);
+			break;
+			
+		case D:
+			node = new Node(tail.row - 1, tail.col, tail.dir);
+			break;
+		}
+		
+		tail.next = node;
+		node.prev = tail;
+		tail = node;
+		size++;
+	}
+	
+	public void draw(Graphics g) {
+		if (this.size <= 0) return;
+		move();
+		for (Node n = head; n != null; n = n.next) {
+			if (n == head) {
+				g.setColor(Color.yellow);
+				n.draw(g);
+			}
+			else {
+				g.setColor(Color.black);
+				n.draw(g);
+			}
+		}
+	}
+	
+	private void move() {	
+		addToHead();
+		deleteFromTail();
+		checkDead();
+	}
+	
+	private void checkDead() {
+		if (head.row < 0 || head.col < 3 || head.row > Yard.ROWS || head.col > Yard.COLS) {
+			
+		}
+			
+	}
+
+	private void deleteFromTail() {
+		if (size == 0) return;
+		tail = tail.prev;
+		tail.next = null;
+	}
+
 	private class Node {
 
 		int w = Yard.BLOCK_SIZE;
 		int h = Yard.BLOCK_SIZE;
 		int row, col;
 		Dir dir = Dir.L;
+		Node next = null;
+		Node prev = null;
 		
-		public Node(int row, int col) {
+		public Node(int row, int col, Dir dir) {
 			super();
 			this.row = row;
 			this.col = col;
@@ -32,9 +125,41 @@ public class Snake {
 		
 		void draw(Graphics g) {
 			Color c = g.getColor();
-			g.setColor(Color.black);
-			g.fillRect(Yard.BLOCK_SIZE * row, Yard.BLOCK_SIZE * col, w, h);
+			g.fillRect(Yard.BLOCK_SIZE * col, Yard.BLOCK_SIZE * row, w, h);
 			g.setColor(c);
+		}
+	}
+	
+	public void eat(Egg e) {
+		if (this.getRect().intersects(e.getRect())) {
+			e.reAppear();
+			this.addToHead();
+		}
+	}
+	
+	private Rectangle getRect() {
+		return new Rectangle(Yard.BLOCK_SIZE * head.col, Yard.BLOCK_SIZE * head.row, head.w, head.h);
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_LEFT:
+			if (head.dir == Dir.R) break;
+			head.dir = Dir.L;
+			break;
+		case KeyEvent.VK_RIGHT:
+			if (head.dir == Dir.L) break;
+			head.dir = Dir.R;
+			break;	
+		case KeyEvent.VK_UP:
+			if (head.dir == Dir.D) break;
+			head.dir = Dir.U;
+			break;
+		case KeyEvent.VK_DOWN:
+			if (head.dir == Dir.U) break;
+			head.dir = Dir.D;
+			break;
 		}
 	}
 }
--- Snake/src/Yard.java
+++ Snake/src/Yard.java
@@ -2,16 +2,21 @@ import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Yard extends Frame {
-	
+
 	public static final int ROWS = 50;
 	public static final int COLS = 50;
 	public static final int BLOCK_SIZE = 10;
 	
-//	Image offscreenImage = null;
+	Snake s = new Snake();
+	Egg e = new Egg();
+	
+	Image offScreenImage = null;
 	
 	public void launch() {
 		this.setLocation(200, 200);
@@ -24,9 +29,11 @@ public class Yard extends Frame {
 			}
 		});
 		this.setVisible(true);
+		this.addKeyListener(new KeyMonitor());
+		
+		new Thread(new PaintThreal()).start();
 	}
-	
-	
+
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -51,6 +58,61 @@ public class Yard extends Frame {
 		}
 		
 		arg0.setColor(c);
+		
+		s.eat(e);
+		e.draw(arg0);
+		s.draw(arg0);
 	}
+	
+	@Override
+	public void update(Graphics g) {
+		if (offScreenImage == null) {
+			offScreenImage = this.createImage(COLS * BLOCK_SIZE, ROWS * BLOCK_SIZE);
+		}
+		Graphics gOff = offScreenImage.getGraphics();
+		paint(gOff);
+		g.drawImage(offScreenImage, 0, 0, null);
+	}
+	
+	private class PaintThreal implements Runnable {
+		public void run() {
+			while(true) {
+				repaint();
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	private class KeyMonitor extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			s.keyPressed(e);
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
